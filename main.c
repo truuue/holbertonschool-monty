db4a4f01db1f9ae380f6dd3a76d10080e2ec8b6c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-08 17:27:26 +0200

    add use of getline function in main to read line by line

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 int main(int argc, char** argv)
 {
 	int fd;
+	char *line = NULL;
+    	size_t size = 0;
+    	ssize_t nread;
+	FILE *file = NULL;
 
 	if (argc != 2)
 	{
@@ -16,8 +20,12 @@ int main(int argc, char** argv)
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	
 
+	file = fdopen(fd, "r");
+	while ((nread = getline(&line, &size, file)) != -1)
+		printf("%s\n", line); //fonction dans un autre fichier .c --> char *temp = strdup(line), strtok(temp, " "), strcmp
+
+	free(line);
 	close(fd);
 	return (0);
 }
