47d9b0c4bc70fb6796d01632e65273086d30dac3
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-09 23:05:56 +0200

    Remove comments and add error print fi malloc fail

--- functions.c
+++ functions.c
@@ -4,15 +4,15 @@ void push(monty_stack_t **stack, unsigned int number)
 {
 	monty_stack_t *new_node = malloc(sizeof(monty_stack_t));
 	if (new_node == NULL)
-		return;
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
 	new_node->n = number;
 	new_node->next = *stack;
 	new_node->prev = NULL;
 	*stack = new_node;
-	//  printf("number in functions = [%d]\n", number);
-	//  printf("number in struct = [%d]\n", new_node->n);
-	//  pall(*stack); //ceci est un test pour verifier si le print fonctionne
 }
 
 void pall(monty_stack_t **stack, unsigned int number)
@@ -30,5 +30,4 @@ void pall(monty_stack_t **stack, unsigned int number)
 	{
 		printf("%d\n", i->n);
 	}
-
 }
