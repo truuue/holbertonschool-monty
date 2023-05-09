bc5e477092208fd8bf151d21026bca808d61bfc5
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-09 21:47:54 +0200

    improve get_ops function

--- get_ops.c
+++ get_ops.c
@@ -1,22 +1,18 @@
 #include "monty.h"
 
-int (*get_ops(char *token))(monty_stack_t **, unsigned int)
+void (*get_ops(char *token))(monty_stack_t **, unsigned int)
 {
-	printf("token get_ops = [%s]\n", token);
-instruction_t opcode_func[] = {
-		{"push", push},
-		{"pall", pall},
-		{NULL, NULL}
-	};
-    int i = 0;
-
-		while (opcode_func[i].opcode != NULL)
-		{
-			if (strcmp(opcode_func[i].opcode, token) == 0)
-			{
-				return (opcode_func[i].f);
-			}
-			i++;
-		}
+	int i = 0;
+	instruction_t opcode_func[] = {
+	    {"push", push},
+	    {"pall", pall},
+	    {NULL, NULL}};
+	
+	while (opcode_func[i].opcode != NULL)
+	{
+		if (strcmp(opcode_func[i].opcode, token) == 0)
+			return (opcode_func[i].f);
+		i++;
+	}
 	return (NULL);
 }
