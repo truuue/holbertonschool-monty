b4816154b2889c32a20fde1a3966a79b5f01e24d
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-11 20:58:13 +0200

    start crafting function
    Co-authored-by: Pizzayolow <Pizzayolow@users.noreply.github.com>
    Co-authored-by: Noah <truuue@users.noreply.github.com>

--- functions2.c
+++ functions2.c
@@ -10,3 +10,20 @@ void nop(monty_stack_t **stack, unsigned int line_nb)
 	(void)**stack;
 	(void)line_nb;
 }
+void swap(monty_stack_t **stack, unsigned int line_nb)
+{
+monty_stack_t *second_node = NULL;
+
+second_node = (*stack)->next;
+
+
+temp = *stack;
+
+if (stack < 2)
+{
+
+}
+
+if (*stack != NULL)
+	
+}	
