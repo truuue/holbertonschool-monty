9f143e630a2ca005ef1e38874e479ee9e886fda1
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-11 10:46:21 -0700

    fix syntax problem in the function

--- functions2.c
+++ functions2.c
@@ -5,4 +5,8 @@
  * @stack: the stack with data
  * @line_nb: the line number with the command
  */
-void nop(monty_stack_t **stack, unsigned int line_nb) {}
+void nop(monty_stack_t **stack, unsigned int line_nb)
+{
+	(void)**stack;
+	(void)line_nb;
+}
