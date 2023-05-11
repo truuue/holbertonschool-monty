f6aab35d55153763fd23d105c11ccf1cce9a8ffe
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-11 10:05:21 -0700

    change line_nb to unsignd int, add char* push variable add call to op_func function with line_nb

--- copy_and_cut.c
+++ copy_and_cut.c
@@ -6,9 +6,9 @@
  * @line_nb: the number of the line containing the command
  * @stack: the stack we need to fill
  */
-void copy_and_cut(char *line, int line_nb, monty_stack_t **stack)
+void copy_and_cut(char *line, unsigned int line_nb, monty_stack_t **stack)
 {
-	char *line_cpy = NULL, *cmd = NULL, *argument = NULL;
+	char *line_cpy = NULL, *cmd = NULL, *argument = NULL, *push = "push";
 	int number = 0;
 	const char *separators = " $\n";
 	void (*op_func)(monty_stack_t **, unsigned int) = NULL;
@@ -17,11 +17,6 @@ void copy_and_cut(char *line, int line_nb, monty_stack_t **stack)
 	if (line != NULL)
 	{
 		line_cpy = strdup(line);
-		// if (line_cpy == NULL)
-		// {
-		// 	global_status = EXIT_FAILURE;
-		// 	return;
-		// }
 		cmd = strtok(line_cpy, separators);
 		argument = strtok(NULL, separators);
 		flag = check_digit(cmd, argument, line_nb, line_cpy);
@@ -38,7 +33,12 @@ void copy_and_cut(char *line, int line_nb, monty_stack_t **stack)
 			return;
 		}
 		else
-			op_func(stack, number);
+		{
+			if (strcmp(cmd, push) == 0)
+				op_func(stack, number);
+			else
+				op_func(stack, line_nb);
+		}
 	}
 	free(line_cpy);
 }
