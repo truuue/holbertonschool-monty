b28f2db3f992d7b4b5cb56b1db953f6b67e4fa31
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-11 06:11:59 -0700

    create print_err2 function

--- print_err.c
+++ print_err.c
@@ -11,3 +11,10 @@ void print_err(char *msg)
 	fprintf(stderr, "%s\n", msg);
 	exit(EXIT_FAILURE);
 }
+
+void print_err2(int line_nb, char *cmd, char *line_cpy)
+{
+	fprintf(stderr, "L%d: usage: %s integer\n", line_nb, cmd);
+	free(line_cpy);
+	global_status = (EXIT_FAILURE);
+}
