a36e82563044615f518051f513614e928e16db2e
truuue <noahvernhet@gmail.com>
2023-05-11 05:09:12 -0700

    Adding the betty's comment in print_err

--- print_err.c
+++ print_err.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/*
+ * print_err - function that takes a pointer to a char msg as input
+ * @msg: print the standard error stream using fprintf
+ *
+ * close the program with a failure status using exit
+ */
 void print_err(char *msg)
 {
 	fprintf(stderr, "%s\n", msg);
