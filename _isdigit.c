f4dd5ff94a556d33767119fad339cee724deb86f
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-11 06:53:01 -0700

    create check_digit function

--- _isdigit.c
+++ _isdigit.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "monty.h"
 
 /**
  * _isdigit - a function that checks for for a digit (0 through 9).
@@ -13,4 +12,36 @@ int _isdigit(int c)
 		return (1);
 	else
 		return (0);
+}
+
+int check_digit(char *cmd, char *argument, int line_nb, char *line_cpy)
+{
+	char *push = "push";
+	int i;
+	int check_digit;
+
+	if (cmd == NULL && argument == NULL)
+	{
+		free(line_cpy);
+		return (1);
+	}
+
+	if ((strcmp(cmd, push) == 0) && argument == NULL)
+	{
+		print_err2(line_nb, cmd, line_cpy);
+		return (1);
+	}
+	else if ((strcmp(cmd, push) == 0) && argument != NULL)
+	{
+		for (i = 0; argument[i] != '\0'; i++)
+		{
+			check_digit = _isdigit(argument[i]);
+			if (check_digit == 0)
+			{
+				print_err2(line_nb, cmd, line_cpy);
+				return (1);
+			}
+		}
+	}
+	return (0);
 }
