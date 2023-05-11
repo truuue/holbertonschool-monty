32329dda7778c7023662bfb20d2ab080d0387a2d
pizzayolow <ralliard.valentin@gmail.com_>
2023-05-11 12:21:27 -0700

    set up of the swap function

--- functions2.c
+++ functions2.c
@@ -16,7 +16,7 @@ void swap(monty_stack_t **stack, unsigned int line_nb)
 	monty_stack_t *temp = *stack;
 	int k = 0;
 
-	if (*stack == NULL || ((*stack)->next) == NULL)
+	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_nb);
 		free_stack(stack);
