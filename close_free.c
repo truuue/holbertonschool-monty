b629875144e526ca6732112bdba7761562351fda
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-11 05:05:17 -0700

    add description to the function as betty style requires

--- close_free.c
+++ close_free.c
@@ -1,5 +1,14 @@
 #include "monty.h"
 
+/**
+ * close_free - a function that frees line and stack and closes file
+ * @line: a line string
+ * @fd: file descriptor
+ * @file: a pointer to a file
+ * @head: a pointer to the top of the stack
+ *
+ * Return: nothing
+ */
 void close_free(char *line, int fd, FILE *file, monty_stack_t *head)
 {
 	if (line != NULL)
