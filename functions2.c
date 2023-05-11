739fb863c75f75ab970ac5ec3dfb19d92ce590f0
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-11 10:42:26 -0700

    new source file with other opcode functions Create the nop function that doesn't do anything

---
+++ functions2.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+/**
+ * nop - This function doesn't do anything
+ * @stack: the stack with data
+ * @line_nb: the line number with the command
+ */
+void nop(monty_stack_t **stack, unsigned int line_nb)
+{
+	return;
+}
