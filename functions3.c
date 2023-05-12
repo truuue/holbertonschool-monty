52140645f404bd4bd9c2583906d96496b8d230bb
truuue <noahvernhet@gmail.com>
2023-05-12 06:28:22 -0700

    Adding the mod function

--- functions3.c
+++ functions3.c
@@ -20,3 +20,29 @@ void mul(monty_stack_t **stack, unsigned int line_nb)
 	temp->next->n = temp->next->n * temp->n;
 	pop(stack, line_nb);
 }
+
+void mod(monty_stack_t **stack, unsigned int line_nb)
+{
+	monty_stack_t *top, *second_top;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_nb);
+                free_stack(stack);
+                exit(EXIT_FAILURE);
+	}
+
+	top = *stack;
+	second_top = top->next;
+
+	if (top->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_nb);
+                free_stack(stack);
+                exit(EXIT_FAILURE);
+	}
+
+	second_top->n %= top->n;
+	*stack = second_top;
+	free(top);
+}
