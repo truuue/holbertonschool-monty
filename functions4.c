9e91522fe65cf23130de3c90a77cf6dcd409d630
truuue <noahvernhet@gmail.com>
2023-05-14 07:45:33 -0700

    Another try for rotr func

--- functions4.c
+++ functions4.c
@@ -10,6 +10,7 @@ void rotr(monty_stack_t **stack, unsigned int line_nb)
 	monty_stack_t *temp = *stack;
     (void)line_nb;
 
+    temp = *stack;
 	if (temp == NULL || temp->next == NULL)
 		return;
 
